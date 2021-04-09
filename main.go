b185ee6165e3354da5693c9df4fd603b487e57e3
hato-poppo <ysng1224@gmail.com>
2021-04-10 08:55:21 +0900

    [Change] Request params.

--- main.go
+++ main.go
@@ -2,80 +2,81 @@ package main
 
 import (
     "log"
-    "net/smtp"
-    "encoding/json"
-    "net/http"
-    "context"
-    "github.com/go-kit/kit/endpoint"
-    httptransport "github.com/go-kit/kit/transport/http"
+	"net/smtp"
+	"encoding/json"
+	"net/http"
+	"context"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 )
 
 type MailService interface {
-    Send(string) int
+	Send(sendRequest) int
 }
 
 type mainService struct{}
 
-func (mainService) Send(s string) int {
-    from_email   := "from address"
-    to_email     := "to address"
-    subject_body := "subject" + "\n\n" + "body"
+func (mainService) Send(req sendRequest) int {
+    from_email   := req.From
+	to_email     := req.To
+    subject_body := "Subject: " + req.Subject + "\n\n" + req.Body
     status       := smtp.SendMail("server:port", nil, from_email, []string{to_email}, []byte(subject_body))
     if status != nil {
-        log.Printf("Error from SMTP Server: %s", status)
-        return 0
+		log.Printf("Error from SMTP Server: %s", status)
+		return 0
     }
-    log.Print("Email Sent Successfully")
-    return 1
+	log.Print("Email Sent Successfully")
+	return 1
 }
 
 // メール送信のリクエスト
 type sendRequest struct {
-  S string `json:"s"`
+	From string `json:"from"`
+	To string `json:"to"`
+	Subject string `json:"subject"`
+	Body string `json:"body"`
 }
 
 // メール送信のレスポンス
 type sendResponse struct {
-  V int `json:"v"`
+	V int `json:"v"`
 }
 
 // メール送信のエンドポイント
 func makeSendEndpoint(svc MailService) endpoint.Endpoint {
-  return func(_ context.Context, request interface{}) (interface{}, error) {
-    req := request.(sendRequest)
-    v := svc.Send(req.S)
-    return sendResponse{v}, nil
-  }
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(sendRequest)
+		v := svc.Send(req)
+		return sendResponse{v}, nil
+	}
 }
 
 func decodeSendRequest(_ context.Context, r *http.Request) (interface{}, error) {
-  var request sendRequest
-  if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-    return nil, err
-  }
-  return request, nil
+	var request sendRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
 }
 
 func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-  return json.NewEncoder(w).Encode(response)
+	return json.NewEncoder(w).Encode(response)
 }
+
 
 func main() {
-    svc := mainService{}
-    // 文字数をカウントする処理のハンドラー
-    sendHandler := httptransport.NewServer(
-      makeSendEndpoint(svc),
-      decodeSendRequest,
-      encodeResponse,
-    )
-    // ハンドラーセットする。
-    http.Handle("/send", sendHandler)
-    // 8081ポートでサーバーを起動する。
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	svc := mainService{}
+
+	// メールを送信する処理のハンドラー
+	sendHandler := httptransport.NewServer(
+		makeSendEndpoint(svc),
+		decodeSendRequest,
+		encodeResponse,
+	)
+
+	// ハンドラーセットする。
+	http.Handle("/mails/send", sendHandler)
+
+	// 8081ポートでサーバーを起動する。
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
-
-// 参考
-// https://netcorecloud.com/tutorials/send-email-through-gmail-smtp-server-using-go/
-
-// どうやらAUTHをサポートしていないらしい…
-// 2021/04/05 13:31:11 Error from SMTP Server: smtp: server doesn't support AUTH
